9e5ed691b95f271ea383f64c4a1299055335badf
Henrik Hovrud <henrik.h@online.no>
2017-03-31 13:49:50 +0200

    Adds simple plotter setup guide

--- src/logger.py
+++ src/logger.py
@@ -12,8 +12,8 @@ class Logger:
         self.templog.setLevel(logging.INFO)
         self.log.setLevel(logging.DEBUG)
 
-        temphandler = RotatingFileHandler("log/templog", maxBytes=25E6, backupCount=14)
-        loghandler = RotatingFileHandler("log/log", maxBytes=2E6, backupCount=2)
+        temphandler = RotatingFileHandler("log/templog", maxBytes=25E6, backupCount=10)
+        loghandler = RotatingFileHandler("log/log", maxBytes=1E6, backupCount=2)
         log_formatter = logging.Formatter('%(asctime)s->  %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S ')
         temp_formatter = logging.Formatter('%(asctime)s-> %(message)s', datefmt='%d/%m/%Y %H:%M:%S ')
         temphandler.setLevel(logging.DEBUG)
---
+++ src/plotting.py
@@ -0,0 +1,26 @@
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
+logfile = "log/templog"
+
+
+with open(logfile, "r") as infile:
+    lines = infile.readlines()
+
+n = len(lines)
+# times = np.zeros(n)
+times = []
+inside_temps = np.zeros(n)
+outside_temps = np.zeros(n)
+
+for i in range(n):
+    data = lines[i].split(">")[1].split(":")
+    # times[i] = round( float(data[0]) )
+    times.append( datetime.datetime.fromtimestamp(float(data[0])) )
+    inside_temps[i] = float(data[1].split("=")[1])
+    outside_temps[i] = float(data[2].split("=")[1])
+
+print("\nEntries: ",n)
+plt.plot(times[-300:], inside_temps[-300:])
+plt.show()
